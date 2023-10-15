35b6f127c3d46631fb4e6f48816c2d181e389baa
Aman Gupta <aman.ciena.com>
2023-10-16 01:25:19 +0530

    Added home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  headline = "Can we increment the count to 3 in 2023 ?"
+  message = "An attempt to cheer for Team India for WC 2023 while demonstrating few most used angular, front end concepts."
+
 
 }
